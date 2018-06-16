71e0cbc45575ce37a783b5e870da45f1315bd5df
Justin <jzschroeder@gmail.com>
2018-06-16 12:24:33 +0200

    removing single sessions

--- src/main/java/com/schroetech/game/AbstractGame.java
+++ src/main/java/com/schroetech/game/AbstractGame.java
@@ -6,10 +6,29 @@
 
 package com.schroetech.game;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lauren
  */
 public abstract class AbstractGame implements IGame {
     
+    private ArrayList<IPlayer> players = new ArrayList();
+    
+    public void addPlayer(IPlayer newPlayer) {
+        players.add(newPlayer);
+    }
+    
+    public void addPlayers(ArrayList<IPlayer> newPlayers) {
+        players.addAll(newPlayers);
+    }
+    
+    public ArrayList<IPlayer> getPlayers() {
+        return players;
+    }
+    
+    public int getNumPlayers() {
+        return players.size();
+    }
 }
