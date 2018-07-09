7eff02aba120a59076b4b55da5c541718ba6087a
Justin <jzschroeder@gmail.com>
2018-07-10 00:19:11 +0200

    Created Dice Test class

--- src/test/java/com/schroetech/game/object/DiceTest.java
+++ src/test/java/com/schroetech/game/object/DiceTest.java
@@ -12,43 +12,30 @@ import static org.junit.Assert.*;
  * @author lauren
  */
 public class DiceTest {
-    
-    /**
-     * Test of roll method, of class Dice.
-     */
-    @Test
-    public void testRoll() {
-        System.out.println("roll");
-        Dice instance = null;
-        int expResult = 0;
-        int result = instance.roll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of setValue and getValue methods, of class Dice.
      */
     @Test
-    public void testSetValue() {
+    public void testSetValueAndGetValue() {
         System.out.println("Testing setValue and getValue");
         Dice die = new Dice(6);
-        
+        int dieValue = die.getValue();
+        assertTrue(dieValue > 0 && dieValue <= 6);
+        die.setValue(7);
+        assertEquals(dieValue, die.getValue());
+        die.setValue(3);
+        assertEquals(3, die.getValue());
     }
 
     /**
      * Test of getNumSides method, of class Dice.
      */
     @Test
-    public void testGetNumSides() {
-        System.out.println("getNumSides");
-        Dice instance = null;
-        int expResult = 0;
-        int result = instance.getNumSides();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetNumFaces() {
+        System.out.println("Testing getNumFaces");
+        Dice die = new Dice(6);
+        assertEquals(6, die.getNumFaces());
     }
     
 }
